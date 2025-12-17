939f77552d6e0133bd369c114fe3189d0708df87
MandeepBasnet <mandeep3basnet@gmail.com>
2025-12-17 20:01:35 +0545

    Fixed missing packages and routing issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,19 +7,22 @@ import { SignupPage } from './pages/auth/SignupPage'
 import { ForgotPassword } from './pages/auth/ForgotPasswordPage'
 import { BrowseCampaigns } from './pages/campaigns/BrowseCampaigns'
 import { CampaignDetail } from './pages/CampaignDetail'
+import { AuthProvider } from './context/AuthContext'
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/campaigns" element={<BrowseCampaigns />} />
-        <Route path="/campaigns/:id" element={<CampaignDetail />} />
-      </Routes>
+      <AuthProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/campaigns" element={<BrowseCampaigns />} />
+          <Route path="/campaigns/:id" element={<CampaignDetail />} />
+        </Routes>
+      </AuthProvider>
     </Router>
   )
 }
